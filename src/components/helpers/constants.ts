7ff747710bec09fae7e28201bb6f5e4e74968fce
Devansh <devansh@Devanshs-MacBook-Air.local>
2024-10-21 01:54:24 +0530

    fixed bug on frontend

--- src/components/helpers/constants.ts
+++ src/components/helpers/constants.ts
@@ -1,5 +1,4 @@
 export const contractAddress="0xd3e4c20E9B4e8e3AE46a30A2eFE9C2cd9006825a"; //Contract on sepolia
-export const ALCHEMY_RPC_URL="https://base-sepolia.g.alchemy.com/v2/cB8d-av2mkW_a4Eftu_eIHsIjzwZOh5S";
 export const ConversionToUsd=2569;
 export const options = {
     weekday: "short" as "short",
